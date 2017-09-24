09b8a89bcc26ec33442c754665f4f1601648a234
Administrator <245561237@qq.com>
2017-09-24 18:01:36 +0800

    ss

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/GanEasy/WechatMessageServe/orm"
@@ -17,22 +19,66 @@ func echoWxCallbackHandler(c echo.Context) error {
 	return err
 }
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+//Home 主页
+func Home(c echo.Context) error {
+	return c.Render(http.StatusOK, "home", "")
+}
+
+//Follow 关注
+func Follow(c echo.Context) error {
+	// return c.Render(http.StatusOK, "follow", "aa")
+	token := c.Param("token")
+	ids := repository.Decode(token)
+	if id := ids[0]; id > 0 {
+		user := orm.User{}
+		user.GetUserByID(id)
+
+		url, err := repository.GetBindQrcode(int(id))
+		if err != nil {
+			return c.String(http.StatusOK, "-1")
+		}
+
+		// Data
+		type Data struct {
+			QrcodeURL string
+		}
+
+		data := &Data{
+			QrcodeURL: url,
+		}
+		return c.Render(http.StatusOK, "follow", data)
+	}
+	return c.String(http.StatusOK, "0")
+}
+
 func main() {
 	orm.DB().AutoMigrate(&orm.User{})
 
 	host := "https://readfollow.com"
-	e := echo.New()
+	host = "http://192.168.1.152:8888"
 
-	e.Pre(middleware.HTTPSRedirect())
-	e.Pre(middleware.HTTPSNonWWWRedirect())
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
+	e := echo.New()
+	e.Renderer = t
+	// e.Pre(middleware.HTTPSRedirect())
+	// e.Pre(middleware.HTTPSNonWWWRedirect())
 	// Middleware
-	// e.Use(middleware.Logger())
-	// e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	// Route => handler
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
+	e.GET("/", Home)
 
 	e.GET("/reg/:email", func(c echo.Context) error {
 		email := c.Param("email")
@@ -40,13 +86,13 @@ func main() {
 		user.GetUserByEmail(email)
 		if !user.Registered && user.Subscribed { // 未注册，已订阅
 
-			// title := "欢迎加入 voous "
-			title := "Welcome to join voous"
+			title := "欢迎加入 voous "
+			// title := "Welcome to join voous"
 			token := repository.Encode([]int{int(user.ID), 1})
-			url, err := repository.GetBindQrcode(int(user.ID))
-			if err != nil {
-				return c.String(http.StatusOK, "-1")
-			}
+			// url, err := repository.GetBindQrcode(int(user.ID))
+			// if err != nil {
+			// 	return c.String(http.StatusOK, "-1")
+			// }
 			// body := fmt.Sprintf(
 			// 	`Hello , Please click the link, <a href="%v/join/%v">to join us</a> !
 
@@ -59,15 +105,12 @@ func main() {
 			body := fmt.Sprintf(
 				`您好 , 请点击链接, <a href="%v/join/%v">加入我们</a> !
 	
-					<br>
-					<img src="%v" />
-					<br>
-					如果您需要使用我们的微信通知，请使用微信扫描二维码 (需要关注公众号)
+					
 					<br>
 
 					我们提供免费的微信消息提醒服务，如需帮助，请直接回复邮件！
 
-					<br><br> <a href="%v/unsubscribed/%v">我不希望再收到任何邮件</a>`, host, token, url, host, token)
+					<br><br> <a href="%v/unsubscribed/%v">我不希望再收到任何邮件</a>`, host, token, host, token)
 			repository.SendEmail(email, title, body)
 
 			user.Invited = true
@@ -87,11 +130,13 @@ func main() {
 			if user.Email != "" && user.Registered == false {
 				user.Registered = true
 				user.Save()
-				return c.String(http.StatusOK, "1")
+				return c.Redirect(http.StatusFound, fmt.Sprintf("/follow/%v", token))
 			}
 		}
 		return c.String(http.StatusOK, "0")
 	})
+
+	e.GET("/follow/:token", Follow)
 
 	e.GET("/unsubscribed/:token", func(c echo.Context) error {
 		token := c.Param("token")
@@ -128,7 +173,7 @@ func main() {
 
 	// e.Static("/", "src")
 	// Start server
-	// e.Logger.Fatal(e.Start(":8888"))
-	e.Logger.Fatal(e.StartAutoTLS(":443"))
+	e.Logger.Fatal(e.Start(":8888"))
+	// e.Logger.Fatal(e.StartAutoTLS(":443"))
 
 }
